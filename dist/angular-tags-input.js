9104501ff3d3a0790fd68ed0474d7e148c14ed06
Umed Khudoiberdiev <zarrhost@gmail.com>
2015-04-04 20:53:10 +0500

    removed missed file usage

--- dist/angular-tags-input.js
+++ dist/angular-tags-input.js
@@ -74,7 +74,7 @@ angular.module("tagsInput").run(["$templateCache", function($templateCache) {$te
             replace: true,
             restrict: 'E',
             require: ['ngModel', 'selectOptions'],
-            templateUrl: '../src/tags-input/src/tags-input.html',
+            templateUrl: 'tags-input.html',
             controller: 'TagsInputCtrl'
         }
     }
